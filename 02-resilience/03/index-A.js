757b6f61d05270db32e59ccc4708117ffff63121
Fernando Soares <fernando.soares@luizalabs.com>
2020-09-30 20:32:52 -0300

    :sparkles: Add resilience exercises

---
+++ 02-resilience/03/index-A.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// add root route to express
+app.get('/', (req, res) => {
+  console.log('OK');
+  res.send('OK - by API A');
+});
+
+// start application server
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
